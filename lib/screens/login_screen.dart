194d0df685d99cf0af01bf5e49215a1f5114fb9a
Anfacibry <49923143+Anfacibry@users.noreply.github.com>
2020-10-31 17:23:24 -0300

    adicionando obscureText em TextFild no campo de senha, para deixar a senha com oculta

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -100,6 +100,7 @@ class _LoginScreenState extends State<LoginScreen> {
                               color: DefaultTheme.lightTheme.iconTheme.color),
                         ),
                       ),
+                      obscureText: true,
                     ),
                   ),
                   Container(
